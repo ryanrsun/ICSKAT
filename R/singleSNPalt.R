7b61bd9c793f33ca650397317f8351177e81fa78
Sun <rsun3@W10D-2R6G333.mdanderson.edu>
2020-10-17 21:40:13 -0500

    First commit

---
+++ R/singleSNPalt.R
@@ -0,0 +1,70 @@
+#' singleSNPalt.R
+#'
+#' Take a matrix of SNPs and get the interval-censored regression p-value for each one separately using either 
+#' survreg() or coxph() with midpoint approximation.
+#'
+#' @param lt n*1 vector of left side of time interval.
+#' @param rt n*1 vector of right side of time interval.
+#' @param tpos_ind n*1 vector of whether the event was observed after follow-up started (t>0).
+#' @param obsInd n*1 vector of whether the event was observed or right-censored (0).
+#' @param xMat non-SNP covariates matrix.
+#' @param gMat n*p genotype matrix.
+#' @param coxph Boolean, whether to fit Cox PH model.
+#' @param survreg Boolean, whether to fit survreg() weibull model.
+#'
+#' @return A list with the elements:
+#' \item{pVec}{p*1 vector of marginal SNP p-values}
+#'
+#' @export
+#' @examples
+#' X <- matrix(data=rnorm(200), nrow=100)
+#' lt <- runif(n=100, min=0, max=5)
+#' rt <- lt + runif(n=100, min=0, max=5)
+#' dmats <- make_IC_dmat(X=X, lt=lt, rt=rt)
+#' null_fit <- skat_fit_null(init_beta=rep(0, 5), left_dmat=dmats$left_dmat,
+#' right_dmat=dmats$right_dmat, obs_ind=rep(1, n), tpos_ind = as.numeric(lt > 0))
+#' ICsingleSNP(left_dmat=dmats$left_dmat, right_dmat=dmats$right_dmat, obs_ind=rep(1, n),
+#' tpos_ind = as.numeric(lt > 0), null_beta=null_fit$beta_fit, Itt=null_fit$Itt, 
+#' gMat=matrix(data=rbinom(n=200*10, size=2, prob=0.3), nrow=200))
+#'
+singleSNPalt <- function(lt, rt, tpos_ind, obs_ind, xMat, gMat, coxph=TRUE, survreg=TRUE) {
+  
+  # make the midpoint approximation
+  midTime <- ifelse(obs_ind == 0, lt, (lt + rt) / 2)
+  midEvent <- obs_ind
+  
+  # make the times for Surv interval2 type
+  leftTime2 <- ifelse(tpos_ind == 0, NA, lt)
+  rightTime2 <- ifelse(obs_ind == 0, Inf, rt)
+  
+  # now apply
+  p <- ncol(xMat)
+  if (coxph) {
+    pvalCox <- apply(gMat, 2, coxphFn, xMat = xMat, midTime = midTime, midEvent = midEvent, p=p)
+  } else {pvalCox <- NA}
+  
+  if (survreg) {
+    pvalSurv <- apply(gMat, 2, survregFn, xMat = xMat, leftTime2 = leftTime2, rightTime2 = rightTime2, p=p)
+  } else {pvalSurv <- NA}
+  
+  # return
+  return(list(pvalCox = pvalCox, pvalSurv = pvalSurv))
+  
+}
+  
+  
+# function to be applied over gMat to get p-values from survreg()
+survregFn <- function(x, xMat, leftTime2, rightTime2, p) {
+  tempMod <- survival::survreg(Surv(time = leftTime2, time2 = rightTime2, type="interval2") ~ 
+                                xMat + x, dist="weibull")
+  pval <- summary(tempMod)$table[p+2, 4]
+  return(pval)
+}
+  
+# function to be applied over gMat to get p-values from coxPH
+coxphFn <- function(x, xMat, midTime, midEvent, p) {
+  tempMod <- survival::coxph(Surv(time = midTime,  event=midEvent) ~ xMat + x)
+  pval <- summary(tempMod)$coefficients[p+1, 5]
+  return(pval)
+}
+  
