48129375dcc505e88356466692265f1d3c202f64
Ryan Sun <ryanrsun@gmail.com>
2020-12-04 00:53:18 -0600

    First commit

---
+++ R/ICSKATO_bootstrap.R
@@ -0,0 +1,104 @@
+#' ICSKATO_bootstrap.R
+#'
+#' The version of ICSKATO to run when bootstrapping to match kurtosis.
+#'
+#' @param icskatOut The output list from ICSKAT().
+#' @param B Number of bootstrap replications.
+#' @param intervalProbs n*(s+1) matrix where n is number of subjects and s is number of visits, probability of falling in each interval.
+#' @param allVisits n*s matrix with all the visit times for each subject.
+#' @param quant_r Quantiles of time from make_IC_dmats, to keep them constant through bootstrapping.
+#' @param null_fit The null fit output from ICSKAT_fit_null.
+#' @param gMat Genotype matrix used in original test.
+#' @param fitAgain Boolean, whether to fit the null model again in each bootstrap.
+#' @param checkpoint Boolean, whether to print every time 100 bootstraps finish.
+#' @param rhoVec Vector of rhos to search over in SKATO.
+#'
+#' @return A list with the elements:
+#' \item{Qrho}{Vector of SKAT test statistics, one for each rho}
+#' \item{kappaPart}{Scalar first term in kappa}
+#'
+#' @export
+#'
+ICSKATO_bootstrap <- function(icskatOut, B, intervalProbs, allVisits, quant_r,
+                              null_fit, gMat, fitAgain, checkpoint=FALSE, rhoVec) {
+
+  # bootstrap
+  # really the only quantity we need is kappa and QrhoBoot
+  bootDF <- data.frame(mu=rep(NA, B), sigma=NA, testStat=NA, pSKAT=NA, kappa=NA)
+  QrhoBoot <- matrix(data=NA, nrow=B, ncol=7)
+  for (boot_it in 1:B) {
+
+    # draw from model
+    draws <- t(sapply(data.frame(t(intervalProbs)), FUN = rmultinom, n=1, size=1))
+    modelTimes <- t(mapply(matchVisit, data.frame(t(draws)), data.frame(t(allVisits))))
+    newLT <- modelTimes[, 1]
+    newRT <- modelTimes[, 2]
+
+    # new design matrix - feed it the old quant_r
+    newDmat <- make_IC_dmat(xMat=xMat, lt=newLT, rt=newRT, quant_r = quant_r)
+    newLeft <- newDmat$left_dmat
+    newRight <- newDmat$right_dmat
+    obs_ind <- as.numeric(newRT < 999)
+    tpos_ind <- as.numeric(newLT> 0)
+
+    # fit the null to get the new Itt
+    # give it the old beta
+    # or should I do the entire null fit again?
+    if (fitAgain) {
+      newNull <- ICSKAT_fit_null(init_beta=as.numeric(null_fit$beta_fit), lt=newLT, rt=newRT,
+                                 left_dmat=newLeft, right_dmat=newRight, runOnce = FALSE,
+                                 obs_ind=obs_ind, tpos_ind=tpos_ind)
+
+      bootSKAT <- ICskat(left_dmat=newLeft, tpos_ind=tpos_ind, obs_ind=obs_ind,
+                         right_dmat=newRight, gMat=gMat, lt=newLT, rt=newRT,
+                         null_beta=as.numeric(newNull$beta_fit), Itt=newNull$Itt)
+    } else {
+
+      newNull <- ICSKAT_fit_null(init_beta=as.numeric(null_fit$beta_fit), lt=newLT, rt=newRT,
+                                 left_dmat=newLeft, right_dmat=newRight, runOnce = TRUE,
+                                 obs_ind=obs_ind, tpos_ind=tpos_ind)
+
+      # run skat on bootstrapped data
+      # give it the old beta but the null Itt
+      bootSKAT <- ICskat(left_dmat=newLeft, tpos_ind=tpos_ind, obs_ind=obs_ind,
+                         right_dmat=newRight, gMat=gMat, lt=newLT, rt=newRT,
+                         null_beta=as.numeric(null_fit$beta_fit), Itt=newNull$Itt)
+    }
+
+    # fix eigenvalues
+    newLambda <- bootSKAT$lambdaQ
+    idx1 <- which(newLambda >= 0)
+    idx2 <- which(newLambda > mean(newLambda[idx1])/100000)
+    newLambda <- newLambda[idx2]
+
+    # record
+    bootDF$mu[boot_it] <- sum(newLambda)
+    bootDF$sigma[boot_it] <- sqrt(2 * sum(newLambda^2))
+    bootDF$testStat[boot_it] <- bootSKAT$skatQ
+    bootDF$pSKAT[boot_it] <- bootSKAT$p_SKAT
+
+    # it turns out the kappa part can be calculated with just sig_mat and the Ugamma
+    kappaPortion <- matrix(data=rep(colSums(bootSKAT$sig_mat), ncol(bootSKAT$sig_mat)), nrow=ncol(bootSKAT$sig_mat), byrow=TRUE) /
+      sum(bootSKAT$sig_mat)
+    kappaVec <- t(as.numeric(bootSKAT$Ugamma)) -
+      t(as.numeric(bootSKAT$Ugamma))  %*% kappaPortion
+    bootDF$kappa[boot_it] <- sum(kappaVec^2)
+    QrhoBoot[boot_it, ] <-  (1 - rhoVec) * as.numeric(bootSKAT$skatQ) +
+      rhoVec * as.numeric(bootSKAT$burdenQ)
+
+    if (boot_it%%100 == 0) {
+      if (checkpoint) {cat(boot_it)}
+    }
+  }
+
+  # calculate the kurtosis of Qrho and of kappa
+  kurtQvec <- rep(0, ncol(QrhoBoot))
+  for (i in 1:length(kurtQvec)) {
+    kurtQvec[i] <- mean((QrhoBoot[, i] - mean(QrhoBoot[, i]))^4) / mean((QrhoBoot[, i] - mean(QrhoBoot[, i]))^2)^2 - 3
+  }
+  kurtKappa <- mean( (bootDF$kappa - mean(bootDF$kappa))^4 ) / mean( (bootDF$kappa - mean(bootDF$kappa))^2 )^2 - 3
+
+  # return
+  return(list(kurtQvec = kurtQvec, kurtKappa = kurtKappa, bootDF = bootDF, QrhoBoot = QrhoBoot))
+}
+
---
+++ R/mixture_kurtosis.R
@@ -0,0 +1,36 @@
+#' mixture_kurtosis.R
+#'
+#' Calculate the kurtosis of the mixture term when performing SKATO with bootstrapped kurtosis estimate.
+#'
+#' @param tempDF1 Generally the bootstrapped kurtosis of the mixture of chi-squares in kappa.
+#' @param tempDF2 Generally 1 because it's for the chi-square1 RV in kappa.
+#' @param v1 Generally the variance of the mixture of chi-squares plus the variance of zeta in kappa.
+#' @param a1 Generally the 1-rho in front of the first part of the kappa term.
+#' @param a2 Generally the tau(rho) term in front of the chi-square1 RV in kappa.
+#' @return Kurtosis (excess kurtosis to be more precise), use df = 12 / kurtosis.
+#'
+#' @export
+#' @examples
+#' mixture_kurtosis(1, 1, 1, 1, 1)
+#'
+mixture_kurtosis <- function(tempDF1, tempDF2, v1, a1, a2) {
+  # df1 is for the first quadratic form plus xi
+  # df2 <- for the chisq_1
+  # v1 is for the first quadratic form plus xi
+  # a1 is 1-rho, a2 is tau(rho)
+  v2 <- 2 * tempDF2
+
+  # fourth central moments
+  S4_1 <- (12 / tempDF1 + 3) * v1^2
+  S4_2 <- (12 / tempDF2 + 3) * v2^2
+  # unlike variance, which can be added for the first quadratic form plus xi + chisq1,
+  # fourth central moment can't just be added
+  S4 <- a1^4 * S4_1 + a2^4 * S4_2 + 6 * a1^2 * a2^2 * v1 * v2
+  S2 <- a1^2 * v1 + a2^2 * v2
+  tempKurt <- S4 / S2^2 - 3
+  # same check as SKAT
+  if (tempKurt < 0) {tempKurt <- 1e-4}
+
+  return(tempKurt)
+}
+
