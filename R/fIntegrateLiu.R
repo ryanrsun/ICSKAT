a884543e905d2eb5b83e3cab9154a19006444ee9
Ryan Sun <ryanrsun@gmail.com>
2020-12-03 20:14:47 -0600

    remove input of unnecessary terms

--- R/fIntegrateLiu.R
+++ R/fIntegrateLiu.R
@@ -5,8 +5,6 @@
 #' @param x Variable to be integrated over, can be a vector or scalar.
 #' @param muK1 Mean of the mixture of chi-squares that are the first part of the kappa variable.
 #' @param sigmaK1 Standard deviation of the mixture of chi-squares that are the first part of the kappa variable.
-#' @param sigmaZeta Standard deviation of the zeta part of the kappa variable.
-#' @param kappaLambda Eigenvalues that weight the mixture of chi-squares that are the first part of the kappa variable.
 #' @param QrhoDF The data frame output from calling QrhoIC().
 #' @param dfK1 The degrees of freedom from the approximated chi-square.
 #'
@@ -24,19 +22,19 @@
 #' tpos_ind = as.numeric(lt > 0), null_beta=null_fit$beta_fit, Itt=null_fit$Itt,
 #' gMat=matrix(data=rbinom(n=200*10, size=2, prob=0.3), nrow=200))
 #' QrhoIC(rhoVec = seq(from=0, to=1, by=0.1), icskatOut = myoutput)
-#' fIntegrateLiu(x=1, muK1=10, sigmaK1=2, sigmaZeta=2, kappaLambda = rep(1, 10), QrhoDF=QrhoDF, dfK1 = 10)
+#' fIntegrateLiu(x=1, muK1=10, sigmaK1=2, kappaLambda = rep(1, 10), QrhoDF=QrhoDF, dfK1 = 10)
 #'
-fIntegrateLiu <- function(x, muK1, sigmaK1, sigmaZeta, kappaLambda, QrhoDF, dfK1) {
-  
+fIntegrateLiu <- function(x, muK1, sigmaK1, QrhoDF, dfK1) {
+
   # needs to be able to take vectorized x
   nr <- nrow(QrhoDF)
   nx <- length(x)
   tauX <- QrhoDF$tauVec %*% t(x)
   allChoiceMat <- (QrhoDF$qMinVec - tauX) / (1 - QrhoDF$rhoVec)
   minVec <- apply(allChoiceMat, 2, min)
-  
+
   # no need for looping because pchisq is vectorized, unlike CompQuadForm::davies()
 	deltaX <- sqrt(2 * dfK1) * (minVec - muK1) / sigmaK1 + dfK1
 	retVec <- pchisq(deltaX, df = dfK1) * dchisq(x, df=1)
 	return(retVec)
-}	
+}
