9ea830aa0f1f8eccebc1fec71b31852adf0d60a2
Ryan Sun <ryanrsun@gmail.com>
2021-02-08 17:03:51 -0600

    fix indentation

--- R/ICSKAT_fit_null.R
+++ R/ICSKAT_fit_null.R
@@ -31,63 +31,63 @@
 #'
 ICSKAT_fit_null <- function(init_beta, left_dmat, obs_ind, tpos_ind, right_dmat, lt, rt, runOnce=FALSE, checkpoint=FALSE, eps=10^(-6)) {
 
-    diff_beta <- 1
-    iter <- 0
-    temp_beta <- init_beta
-    while( !is.nan(diff_beta) & diff_beta > eps & diff_beta<1000) {
-        # Cumulative hazard under null
-        H_L <- exp(left_dmat %*% temp_beta)
-        H_R <- exp(right_dmat %*% temp_beta)
-        # Survival term
-        SL <- ifelse(lt == 0, 1, exp(-H_L))
-        SR <- ifelse(rt == 999, 0, exp(-H_R))
-        SR[!is.finite(SR)] <- 0
-        A <- SL - SR
-        # sometimes A is 0
-        A[which(A == 0)] <- min(A[which(A > 0)])
+  diff_beta <- 1
+  iter <- 0
+  temp_beta <- init_beta
+  while( !is.nan(diff_beta) & diff_beta > eps & diff_beta<1000) {
+    # Cumulative hazard under null
+    H_L <- exp(left_dmat %*% temp_beta)
+    H_R <- exp(right_dmat %*% temp_beta)
+    # Survival term
+    SL <- ifelse(lt == 0, 1, exp(-H_L))
+    SR <- ifelse(rt == 999, 0, exp(-H_R))
+    SR[!is.finite(SR)] <- 0
+    A <- SL - SR
+    # sometimes A is 0
+    A[which(A == 0)] <- min(A[which(A > 0)])
 
-        # score vector
-        U_term1 <- sweep(t(left_dmat), 2, ifelse(lt == 0, 0, exp(-H_L) * -H_L), FUN="*")
-        U_term2 <- sweep(t(right_dmat), 2, ifelse(rt == 999, 0, exp(-H_R) * -H_R), FUN="*")
-        U_term2[is.na(U_term2)] <- 0
-        uVec <- rowSums( sweep(U_term1 - U_term2, 2, A, FUN="/") )
+    # score vector
+    U_term1 <- sweep(t(left_dmat), 2, ifelse(lt == 0, 0, exp(-H_L) * -H_L), FUN="*")
+    U_term2 <- sweep(t(right_dmat), 2, ifelse(rt == 999, 0, exp(-H_R) * -H_R), FUN="*")
+    U_term2[is.na(U_term2)] <- 0
+    uVec <- rowSums( sweep(U_term1 - U_term2, 2, A, FUN="/") )
 
-        # information matrix
-        I_term1 <- crossprod(left_dmat, sweep(left_dmat, 1, tpos_ind * as.numeric((-H_L * exp(-H_L) + H_L^2 * exp(-H_L)) / A), FUN="*"))
-        # sometimes H_R is so large that when it gets squared it goes to Inf and then multiplied
-        # by exp(-H_R) it goes to NaN
-        check_Iterm2 <- (H_R * exp(-H_R) - H_R^2 * exp(-H_R))
-        check_Iterm2[which(is.nan(check_Iterm2))] <- 0
-        I_term2 <- crossprod(right_dmat, sweep(right_dmat, 1, obs_ind * as.numeric(check_Iterm2 / A), FUN="*"))
-        check_tempterm <- (H_R * exp(-H_R))
-        check_tempterm[which(is.nan(check_tempterm))] <- 0
-	temp_term <- sweep(t(left_dmat), 2, tpos_ind * as.numeric((H_L * exp(-H_L)) / A), FUN="*") -
-	sweep(t(right_dmat), 2, obs_ind * as.numeric(check_tempterm / A), FUN="*")
-        I_term3 <- temp_term %*% t(temp_term)
-        iMat <- I_term1 + I_term2  - I_term3
+    # information matrix
+    I_term1 <- crossprod(left_dmat, sweep(left_dmat, 1, tpos_ind * as.numeric((-H_L * exp(-H_L) + H_L^2 * exp(-H_L)) / A), FUN="*"))
+    # sometimes H_R is so large that when it gets squared it goes to Inf and then multiplied
+    # by exp(-H_R) it goes to NaN
+    check_Iterm2 <- (H_R * exp(-H_R) - H_R^2 * exp(-H_R))
+    check_Iterm2[which(is.nan(check_Iterm2))] <- 0
+    I_term2 <- crossprod(right_dmat, sweep(right_dmat, 1, obs_ind * as.numeric(check_Iterm2 / A), FUN="*"))
+    check_tempterm <- (H_R * exp(-H_R))
+    check_tempterm[which(is.nan(check_tempterm))] <- 0
+    temp_term <- sweep(t(left_dmat), 2, tpos_ind * as.numeric((H_L * exp(-H_L)) / A), FUN="*") -
+    sweep(t(right_dmat), 2, obs_ind * as.numeric(check_tempterm / A), FUN="*")
+    I_term3 <- temp_term %*% t(temp_term)
+    iMat <- I_term1 + I_term2  - I_term3
 
-				# sometimes iMat is singular
-				solvedImat <- tryCatch(solve(iMat), error=function(e) e)
-				if (class(solvedImat)[1] %in% c("simpleError")) {
-					return(list(beta_fit=NA, iter=iter, diff_beta=diff_beta, Itt=NA, err=1, errMsg="iMat singular, try different initial values"))
-				}
-				if (length(which(is.na(solvedImat))) > 0)
-				{
-					return(list(beta_fit=NA, iter=iter, diff_beta=diff_beta, Itt=NA, err=1, errMsg="iMat inverse has NaN, try different initial values"))
-				}
-        beta_new <- temp_beta - t(uVec) %*% solve(iMat)
-        diff_beta <- (beta_new - temp_beta) %*% t(beta_new - temp_beta)
-        temp_beta <- as.numeric(beta_new)
-        iter <- iter + 1
-        if (runOnce) {break}
+    # sometimes iMat is singular
+    solvedImat <- tryCatch(solve(iMat), error=function(e) e)
+    if (class(solvedImat)[1] %in% c("simpleError")) {
+      return(list(beta_fit=NA, iter=iter, diff_beta=diff_beta, Itt=NA, err=1, errMsg="iMat singular, try different initial values"))
+    }
+    if (length(which(is.na(solvedImat))) > 0)
+    {
+      return(list(beta_fit=NA, iter=iter, diff_beta=diff_beta, Itt=NA, err=1, errMsg="iMat inverse has NaN, try different initial values"))
+    }
+    beta_new <- temp_beta - t(uVec) %*% solve(iMat)
+    diff_beta <- (beta_new - temp_beta) %*% t(beta_new - temp_beta)
+    temp_beta <- as.numeric(beta_new)
+    iter <- iter + 1
+    if (runOnce) {break}
 
-				# very rarely does it get stuck
-				if (iter > 100) {
-					return(list(beta_fit=NA, iter=iter, diff_beta=diff_beta, Itt=NA, err=1, errMsg="Too many iterations, try different initial values"))
-				}
-
-				if(checkpoint) {cat("iter ", iter, "\n")}
+    # very rarely does it get stuck
+    if (iter > 100) {
+      return(list(beta_fit=NA, iter=iter, diff_beta=diff_beta, Itt=NA, err=1, errMsg="Too many iterations, try different initial values"))
     }
 
-    return(list(beta_fit=beta_new, iter=iter, diff_beta=diff_beta, Itt=iMat, err=0, errMsg=""))
+    if(checkpoint) {cat("iter ", iter, "\n")}
+  }
+
+  return(list(beta_fit=beta_new, iter=iter, diff_beta=diff_beta, Itt=iMat, err=0, errMsg=""))
 }
