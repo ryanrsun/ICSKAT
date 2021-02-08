f9b64a035f4417d8ec0a60f92165e06813d56a26
Ryan Sun <ryanrsun@gmail.com>
2021-02-08 16:36:48 -0600

    change indentation

--- R/ICSKAT.R
+++ R/ICSKAT.R
@@ -62,56 +62,56 @@ ICskat <- function(left_dmat, right_dmat, lt, rt, obs_ind, tpos_ind, gMat, null_
   rm(Ug_sweepTerm)
 
 
-	# The Igg term
-	ggTerm1 <- tpos_ind * as.numeric((-H_L * exp(-H_L) + H_L^2 * exp(-H_L)) / A)
-	ggTerm2 <- obs_ind * as.numeric(H_R * exp(-H_R) - H_R^2 * exp(-H_R)) / A
-	ggTerm2[which(is.nan(ggTerm2))] <- 0
-	ggTerm3 <- ( (tpos_ind * as.numeric((H_L * exp(-H_L))) - obs_ind * as.numeric((H_R * exp(-H_R)))) / A )^2
-	ggTerm <- ggTerm1 + ggTerm2 - ggTerm3
-	IggHalf <- sweep(gMat, 1, ggTerm, FUN="*")
-	# have to do this to make it a double matrix
-	gMat[1, 1] <- 1.0 * gMat[1, 1]
-	Igg <- eigenMapMatMultCrossTwo(gMat, IggHalf)
-	# rm to save ram
-	rm(ggTerm1, ggTerm3, ggTerm, IggHalf)
+  # The Igg term
+  ggTerm1 <- tpos_ind * as.numeric((-H_L * exp(-H_L) + H_L^2 * exp(-H_L)) / A)
+  ggTerm2 <- obs_ind * as.numeric(H_R * exp(-H_R) - H_R^2 * exp(-H_R)) / A
+  ggTerm2[which(is.nan(ggTerm2))] <- 0
+  ggTerm3 <- ( (tpos_ind * as.numeric((H_L * exp(-H_L))) - obs_ind * as.numeric((H_R * exp(-H_R)))) / A )^2
+  ggTerm <- ggTerm1 + ggTerm2 - ggTerm3
+  IggHalf <- sweep(gMat, 1, ggTerm, FUN="*")
+  # have to do this to make it a double matrix
+  gMat[1, 1] <- 1.0 * gMat[1, 1]
+  Igg <- eigenMapMatMultCrossTwo(gMat, IggHalf)
+  # rm to save ram
+  rm(ggTerm1, ggTerm3, ggTerm, IggHalf)
 
   # off-diagonals for Igtheta
-	gtTermL <- tpos_ind * as.numeric((-H_L * exp(-H_L) + H_L^2 * exp(-H_L)) / A) +
-	  tpos_ind * as.numeric((H_L * exp(-H_L)) / A) * (tpos_ind * as.numeric((-H_L * exp(-H_L)) / A) + obs_ind * as.numeric((H_R * exp(-H_R)) / A))
-	gtTermR <- obs_ind * ggTerm2 - obs_ind * as.numeric((H_R * exp(-H_R)) / A) *
-	  (tpos_ind * as.numeric((-H_L * exp(-H_L)) / A) + obs_ind * as.numeric((H_R * exp(-H_R)) / A))
-	gtHalfL <- sweep(left_dmat, 1, gtTermL, FUN="*")
-	gtHalfR <- sweep(right_dmat, 1, gtTermR, FUN="*")
-	Igt <- eigenMapMatMultCrossTwo(gMat, gtHalfL) + eigenMapMatMultCrossTwo(gMat, gtHalfR)
-	# rm to save ram
-	rm(gtTermL, gtTermR, ggTerm2, gtHalfL, gtHalfR)
+  gtTermL <- tpos_ind * as.numeric((-H_L * exp(-H_L) + H_L^2 * exp(-H_L)) / A) +
+    tpos_ind * as.numeric((H_L * exp(-H_L)) / A) * (tpos_ind * as.numeric((-H_L * exp(-H_L)) / A) + obs_ind * as.numeric((H_R * exp(-H_R)) / A))
+  gtTermR <- obs_ind * ggTerm2 - obs_ind * as.numeric((H_R * exp(-H_R)) / A) *
+    (tpos_ind * as.numeric((-H_L * exp(-H_L)) / A) + obs_ind * as.numeric((H_R * exp(-H_R)) / A))
+  gtHalfL <- sweep(left_dmat, 1, gtTermL, FUN="*")
+  gtHalfR <- sweep(right_dmat, 1, gtTermR, FUN="*")
+  Igt <- eigenMapMatMultCrossTwo(gMat, gtHalfL) + eigenMapMatMultCrossTwo(gMat, gtHalfR)
+  # rm to save ram
+  rm(gtTermL, gtTermR, ggTerm2, gtHalfL, gtHalfR)
 
   # we just need the Igg portion of the inverse
- 	# make sure there are no NA
-	if (length(which(is.na(Igg))) > 0 | length(which(is.na(Igt))) > 0 | length(which(is.na(Itt))) > 0) {
-		return(list(lambdaQ=NA, p_SKAT=NA, p_burden=NA, skatQ=NA, Ugamma=Ugamma, null_beta = null_beta,
+  # make sure there are no NA
+  if (length(which(is.na(Igg))) > 0 | length(which(is.na(Igt))) > 0 | length(which(is.na(Itt))) > 0) {
+  	return(list(lambdaQ=NA, p_SKAT=NA, p_burden=NA, skatQ=NA, Ugamma=Ugamma, null_beta = null_beta,
               burdenQ=NA, sig_mat = NA, complex=NA, err=99, errMsg="NA in variance matrix"))
-	} 
-	sig_mat <- (-Igg) - (-Igt) %*% solve(-Itt) %*% t(-Igt)
-	if (length(which(is.na(sig_mat))) > 0) {
-		return(list(lambdaQ=NA, p_SKAT=NA, p_burden=NA, skatQ=NA, Ugamma=Ugamma, null_beta = null_beta,
+  } 
+  sig_mat <- (-Igg) - (-Igt) %*% solve(-Itt) %*% t(-Igt)
+  if (length(which(is.na(sig_mat))) > 0) {
+    return(list(lambdaQ=NA, p_SKAT=NA, p_burden=NA, skatQ=NA, Ugamma=Ugamma, null_beta = null_beta,
               burdenQ=NA, sig_mat = NA, complex=NA, err=99, errMsg="NA in variance matrix"))
   } 
   skatQ <- t(Ugamma) %*% Ugamma
   burdenQ <- (sum(Ugamma))^2
 
-	errCode <- 0
-	errMsg <- ""
+  errCode <- 0
+  errMsg <- ""
   if (pvalue) {
     lambdaQ <- eigen(sig_mat)$values
     p_SKAT <- CompQuadForm::davies(q=skatQ, lambda=lambdaQ, delta=rep(0,length(lambdaQ)), acc=1e-7)$Qq
- 		# as noted in the CompQuadForm documentation, sometimes you need to play with acc or lim parameters
-		if (!is.na(p_SKAT)) {
-			if (p_SKAT > 1) {
-				paramDF <- data.frame(expand.grid(lim = c(10000, 20000, 50000), acc=c(1e-7, 1e-6, 1e-5, 1e-4)))
-				paramCounter <- 1
-				while(p_SKAT > 1) {
-					tempLim <- paramDF$lim[paramCounter]
+    # as noted in the CompQuadForm documentation, sometimes you need to play with acc or lim parameters
+    if (!is.na(p_SKAT)) {
+		  if (p_SKAT > 1) {
+			  paramDF <- data.frame(expand.grid(lim = c(10000, 20000, 50000), acc=c(1e-7, 1e-6, 1e-5, 1e-4)))
+			  paramCounter <- 1
+			  while(p_SKAT > 1) {
+				  tempLim <- paramDF$lim[paramCounter]
 					tempAcc <- paramDF$acc[paramCounter]
 					p_SKAT <- CompQuadForm::davies(q=skatQ, lambda=lambdaQ, delta=rep(0,length(lambdaQ)), acc=tempAcc, lim=tempLim)$Qq
 					paramCounter <- paramCounter + 1
