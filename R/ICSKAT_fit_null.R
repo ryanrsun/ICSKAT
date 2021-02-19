adb0f0472233c93763014e13d8f938cfcd0fa197
Ryan Sun <ryanrsun@gmail.com>
2021-02-18 23:28:35 -0600

    more security in using obs_ind

--- R/ICSKAT_fit_null.R
+++ R/ICSKAT_fit_null.R
@@ -45,8 +45,8 @@ ICSKAT_fit_null <- function(init_beta, left_dmat, right_dmat, obs_ind, tpos_ind,
   temp_beta <- init_beta
   while( !is.nan(diff_beta) & diff_beta > eps & diff_beta<1000) {
     # Cumulative hazard under null
-    H_L <- exp(left_dmat %*% temp_beta)
-    H_R <- exp(right_dmat %*% temp_beta)
+    H_L <- as.numeric(exp(left_dmat %*% temp_beta))
+    H_R <- as.numeric(exp(right_dmat %*% temp_beta))
     # Survival term
     SL <- ifelse(tpos_ind == 0, 1, exp(-H_L))
     SR <- ifelse(obs_ind == 0, 0, exp(-H_R))
@@ -65,10 +65,10 @@ ICSKAT_fit_null <- function(init_beta, left_dmat, right_dmat, obs_ind, tpos_ind,
     I_term1 <- crossprod(left_dmat, sweep(left_dmat, 1, tpos_ind * as.numeric((-H_L * exp(-H_L) + H_L^2 * exp(-H_L)) / A), FUN="*"))
     # sometimes H_R is so large that when it gets squared it goes to Inf and then multiplied
     # by exp(-H_R) it goes to NaN
-    check_Iterm2 <- (H_R * exp(-H_R) - H_R^2 * exp(-H_R))
+    check_Iterm2 <- ifelse(obs_ind == 0, 0, H_R * exp(-H_R) - H_R^2 * exp(-H_R))
     check_Iterm2[which(is.nan(check_Iterm2))] <- 0
     I_term2 <- crossprod(right_dmat, sweep(right_dmat, 1, obs_ind * as.numeric(check_Iterm2 / A), FUN="*"))
-    check_tempterm <- (H_R * exp(-H_R))
+    check_tempterm <- ifelse(obs_ind == 0, 0, H_R * exp(-H_R))
     check_tempterm[which(is.nan(check_tempterm))] <- 0
     temp_term <- sweep(t(left_dmat), 2, tpos_ind * as.numeric((H_L * exp(-H_L)) / A), FUN="*") -
     sweep(t(right_dmat), 2, obs_ind * as.numeric(check_tempterm / A), FUN="*")
