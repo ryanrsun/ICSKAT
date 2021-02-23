4a3794324e2dd4f3b79863b163fccc63f47864df
Ryan Sun <ryanrsun@gmail.com>
2021-02-22 23:57:12 -0600

    major overhaul to remove legacy code

--- R/ICSKATPO.R
+++ R/ICSKATPO.R
@@ -21,7 +21,6 @@
 #' \item{burdenQ}{Burden test statistic.}
 #' \item{err}{err=1 for a bad null fit.}
 #' \item{errMsg}{Describes the error.}
-3''
 #' @export
 #' @examples
 #' gMat <- matrix(data=rbinom(n=200, size=2, prob=0.3), nrow=100)
@@ -36,110 +35,98 @@
 #' tpos_ind <- as.numeric(lt > 0)
 #' obs_ind <- as.numeric(rt != Inf)
 #' dmats <- make_IC_dmat(xMat, lt, rt)
-#' nullFit <- ICSKAT_fit_null(init_beta = rep(0, 5), left_dmat = dmats$left_dmat, right_dmat=dmats$right_dmat, 
+#' nullFit <- ICSKAT_fit_null(init_beta = rep(0.1, 5), left_dmat = dmats$left_dmat, right_dmat=dmats$right_dmat,
 #' obs_ind = obs_ind, tpos_ind = tpos_ind, lt = lt, rt = rt)
 #' ICskatPO(left_dmat = dmats$left_dmat, right_dmat=dmats$right_dmat, lt = lt, rt = rt,
 #' obs_ind = obs_ind, tpos_ind = tpos_ind, gMat = gMat, null_beta = nullFit$beta_fit, Itt = nullFit$Itt)
 #'
 ICskatPO <- function(left_dmat, right_dmat, lt, rt, obs_ind, tpos_ind, gMat, null_beta, Itt) {
 
-  # corrected
-  etaL <- left_dmat %*% null_beta 
+  # linear predictor
+  etaL <- left_dmat %*% null_beta
   etaR <- right_dmat %*% null_beta
-  
-  # Survival term
+
+  # survival term
   SL <- ifelse(tpos_ind == 0, 1, expit(-etaL))
   SR <- ifelse(obs_ind == 0, 0, expit(-etaR))
   SR[!is.finite(SR)] <- 0
-  SLSR <- SL - SR
+  A <- SL - SR
   # sometimes A is 0
-  # set it at 10^(-100) because you can square that and R will not round it to 0
-  #SLSR[which(SLSR <= 0)] <- min(SLSR[which(SLSR > 0)])
-  #SLSR[which(SLSR < 10^(-100))] <- 10^(-100)
-  SLSR[which(SLSR == 0)] <- min(SLSR[which(SLSR > 0)])
- 
-  # if lt=0 or rt=999, we swap 0 because -expit(etaL) is the survival and should be 0 or 1. 
-  dGinvLdEta <- ifelse(tpos_ind == 0, 0, -expit(-etaL) * (1 - expit(-etaL)))
-  dGinvRdEta <- ifelse(obs_ind == 0, 0, -expit(-etaR) * (1 - expit(-etaR))) 
-  # put a 1 in here to remind that we've taken the ZL and ZR out of these two terms
-  # these are all terms that will be put in the final matrix as sweep(t(Z), 2, toSweep) %*% Z
-  dEtaLdAlpha <- 1 
-  dEtaRdAlpha <- 1 
-  
+  A[which(A == 0)] <- min(A[which(A > 0)])
+
+  # expit(-eta) = S(t) = 1 when t=0, so 1 - expit(-eta) = 1 - S(t) = 0
+  # we could also use -expit(-etaL) * (1 - expit(-etaL))
+  dGinvLdEta <- ifelse(tpos_ind == 0, 0, -SL * (1 - SL))
+  dGinvRdEta <- ifelse(obs_ind == 0, 0, -SR * (1 - SR))
+
   # second derivative terms
-  # if lt=0 or rt=999, we swap 0 because -expit(etaL) is the survival and should be 0 or 1. 
-  # also, if checkNumR is -Inf, that means the denominator is like -Inf^2, so that's why we set it to 0 so the 
-  # quotient will be correctly 0.	
-  checkNumR <- exp(-etaR) - exp(-2 * etaR)
-  numR <- ifelse(exp(-2 * etaR) == Inf, 0, checkNumR)
-  checkNumL <- exp(-etaL) - exp(-2 * etaL)
-  numL <- ifelse(exp(-2 * etaL) == Inf, 0, checkNumL)
-  d2GinvLdEta2 <- ifelse(tpos_ind == 0, 0, numL / (1 + exp(-etaL))^3)  
-  d2GinvRdEta2 <- ifelse(obs_ind == 0, 0, numR / (1 + exp(-etaR))^3)
- 
+  # again, there's an S(T) and a 1 - S(T) in these terms
+  # we could also use (exp(-etaL) - exp(-2 * etaL)) / (1 + exp(-etaL))^3
+  d2GinvLdEta2 <- ifelse(tpos_ind == 0, 0, 2 * SL^3 - 3 * SL^2 + SL)
+  d2GinvRdEta2 <- ifelse(obs_ind == 0, 0, 2 * SR^3 - 3 * SR^2 + SR)
+
   # score vector
-  # this works fine with minimal checking because when SL or SR is very small, both the numerator and denominator are on the
-  # same scale as long as SLSR is not allowed to be 0.
-  UgammaSweep <- (dGinvLdEta - dGinvRdEta) / SLSR
+  UgammaSweep <- (dGinvLdEta - dGinvRdEta) / A
   Ugamma <- rowSums( sweep(t(gMat), MARGIN = 2, STATS = UgammaSweep, FUN = "*") )
   if (length(which(is.na(UgammaSweep))) > 0 | length(which(is.na(Ugamma))) > 0) {
     return(list(p_SKAT=NA, p_burden=NA, skatQ=NA, burdenQ=NA, sig_mat = NA, complex=NA, err=1, errMsg="bad null fit, try different starting values"))
   }
 
   # Igg
-  #IggSweepCheck <- (d2GinvLdEta2 - d2GinvRdEta2) / SLSR - UgammaSweep^2
-  #IggMax <- min( max(abs(IggSweepCheck[which(abs(IggSweepCheck) != Inf)])), 10^20 )
-  #IggSweep <- ifelse(abs(IggSweepCheck) > IggMax, IggMax * sign(IggSweepCheck), IggSweepCheck)
-  IggSweep <- (d2GinvLdEta2 - d2GinvRdEta2) / SLSR - UgammaSweep^2	
+  IggSweep <- (d2GinvLdEta2 - d2GinvRdEta2) / A - UgammaSweep^2
   Igg <- sweep(t(gMat), 2, IggSweep, FUN = "*") %*% gMat
   if (length(which(is.na(Igg))) > 0) {
     return(list(p_SKAT=NA, p_burden=NA, skatQ=NA, burdenQ=NA, sig_mat = NA, complex=NA, err=1, errMsg="bad null fit, try different starting values"))
   }
-  
+
   # Iga
-  # we can get in trouble when SL - SR is very small, in that case the second term is kind of like dGinvLdEta / SLSR
-  #checkIgaSweepL <- ifelse(lt == 0, 0, d2GinvLdEta2 * dEtaLdAlpha / SLSR - (dGinvLdEta - dGinvRdEta) * dGinvLdEta * dEtaLdAlpha / SLSR^2)
-  #IgaSweepL1 <- ifelse(checkIgaSweepL > 10^100 | is.na(checkIgaSweepL), d2GinvLdEta2 * dEtaLdAlpha / SLSR -  dGinvLdEta * dEtaLdAlpha / SLSR, checkIgaSweepL)
-  #IgaSweepL <- ifelse(abs(IgaSweepL1) > 10^30, sign(IgaSweepL1) * 10^30, IgaSweepL1) 	
-  #checkIgaSweepR <- ifelse(rt == 999, 0, d2GinvRdEta2 * dEtaRdAlpha / SLSR - (dGinvLdEta - dGinvRdEta) * dGinvRdEta * dEtaRdAlpha / SLSR^2)
-  #IgaSweepR1 <- ifelse(checkIgaSweepR > 10^100 | is.na(checkIgaSweepR), d2GinvRdEta2 * dEtaRdAlpha / SLSR - dGinvRdEta * dEtaRdAlpha / SLSR, checkIgaSweepR)
-  #IgaSweepR	<- ifelse(abs(IgaSweepR1) > 10^30, sign(IgaSweepR1) * 10^30, IgaSweepR1)
-  IgaSweepL <- ifelse(tpos_ind == 0, 0, d2GinvLdEta2 * dEtaLdAlpha / SLSR - (dGinvLdEta - dGinvRdEta) * dGinvLdEta * dEtaLdAlpha / SLSR^2)
-  IgaSweepR <- ifelse(obs_ind == 0, 0, d2GinvRdEta2 * dEtaRdAlpha / SLSR - (dGinvLdEta - dGinvRdEta) * dGinvRdEta * dEtaRdAlpha / SLSR^2)
-  IgaL <- sweep(t(gMat), 2, IgaSweepL, FUN = "*") %*% ZL
-  IgaR <- sweep(t(gMat), 2, IgaSweepR, FUN = "*") %*% ZR
-  Iga <- IgaL - IgaR
-  
-  # Igb
-  Igb <- sweep(t(gMat), 2, IggSweep, FUN = "*") %*% xMat
- 
+  IgtSweepL <- ifelse(tpos_ind == 0, 0, d2GinvLdEta2  / A - (dGinvLdEta - dGinvRdEta) * dGinvLdEta  / A^2)
+  IgtSweepR <- ifelse(obs_ind == 0, 0, d2GinvRdEta2  / A - (dGinvLdEta - dGinvRdEta) * dGinvRdEta  / A^2)
+  IgtL <- sweep(t(gMat), 2, IgtSweepL, FUN = "*") %*% left_dmat
+  IgtR <- sweep(t(gMat), 2, IgtSweepR, FUN = "*") %*% right_dmat
+  Igt <- IgtL - IgtR
+
   # remove to save RAM?
   rm(UgammaSweep)
   rm(IggSweep)
-  rm(IgaSweepL)
-  rm(IgaSweepR)
-  rm(IgaL)
-  rm(IgaR) 
+  rm(IgtSweepL)
+  rm(IgtSweepR)
+  rm(IgtL)
+  rm(IgtR)
 
-  # off diagonal
-  offDiag <- cbind(Igb, Iga)
-  if (length(which(is.na(offDiag))) > 0) {
-    return(list(p_SKAT=NA, p_burden=NA, skatQ=NA, burdenQ=NA, sig_mat = NA, complex=NA, err=1, errMsg="bad null fit, try different starting values"))
-  }
-  sig_mat <- (-Igg) - (-offDiag) %*% solve(-Itt) %*% t(-offDiag)
+  sig_mat <- (-Igg) - (-Igt) %*% solve(-Itt) %*% t(-Igt)
   skatQ <- t(Ugamma) %*% Ugamma
   # final check for bad fit
   if (length(which(is.na(sig_mat))) > 0 | is.na(skatQ)) {
     return(list(p_SKAT=NA, p_burden=NA, skatQ=NA, burdenQ=NA, sig_mat = NA, complex=NA, err=1, errMsg="bad null fit, try different starting values"))
-  } 
+  }
   lambdaQ <- eigen(sig_mat)$values
   p_SKAT <- CompQuadForm::davies(q=skatQ, lambda=lambdaQ, delta=rep(0,length(lambdaQ)), acc=1e-7)$Qq
-  
+
+  # errors?
+  errCode <- 0
+  errMsg <- ""
+  if (p_SKAT > 1) {
+    paramDF <- data.frame(expand.grid(lim = c(10000, 20000, 50000), acc=c(1e-7, 1e-6, 1e-5, 1e-4)))
+    paramCounter <- 1
+    # normal adjustment for davies function
+    while(p_SKAT > 1) {
+      tempLim <- paramDF$lim[paramCounter]
+      tempAcc <- paramDF$acc[paramCounter]
+      p_SKAT <- CompQuadForm::davies(q=skatQ, lambda=lambdaQ, delta=rep(0,length(lambdaQ)), acc=tempAcc, lim=tempLim)$Qq
+      paramCounter <- paramCounter + 1
+      if (paramCounter > nrow(paramDF)) {break}
+    }
+    errCode <- 22
+    errMsg <- "Had to adjust parameters on CompQuadForm."
+  }
+
+  # burden
   burdenQ <- (sum(Ugamma))^2
-  B_burden= burdenQ / sum(sig_mat);
-  p_burden=1-pchisq(B_burden,df=1)
-  
-  return(list(p_SKAT=p_SKAT, p_burden=p_burden, skatQ=skatQ, burdenQ=burdenQ, sig_mat = sig_mat, complex=is.complex(lambdaQ), err=0, errMsg=""))
+  B_burden = burdenQ / sum(sig_mat);
+  p_burden = 1- pchisq(B_burden,df=1)
+
+  return(list(p_SKAT=p_SKAT, p_burden=p_burden, skatQ=skatQ, burdenQ=burdenQ, sig_mat = sig_mat, complex=is.complex(lambdaQ), err=errCode, errMsg=errMsg))
 }
 
 
--- R/ICSKAT_fit_null_PO.R
+++ R/ICSKAT_fit_null_PO.R
@@ -33,77 +33,77 @@
 #' tpos_ind <- as.numeric(lt > 0)
 #' obs_ind <- as.numeric(rt != Inf)
 #' dmats <- make_IC_dmat(xMat, lt, rt)
-#' ICSKAT_fit_null_PO(init_beta = rep(0, 5), left_dmat = dmats$left_dmat, right_dmat=dmats$right_dmat, 
+#' ICSKAT_fit_null_PO(init_beta = rep(0, 5), left_dmat = dmats$left_dmat, right_dmat=dmats$right_dmat,
 #' obs_ind = obs_ind, tpos_ind = tpos_ind, lt = lt, rt = rt
 #'
-ICSKAT_fit_null_PO <- function(init_beta, left_dmat, right_dmat, obs_ind, tpos_ind, lt, rt, checkpoint=FALSE, eps=10^(-6)) {
-  
+ICSKAT_fit_null_PO <- function(init_beta, left_dmat, right_dmat, obs_ind, tpos_ind, lt, rt, ZL, ZR, xMat, checkpoint=FALSE, eps=10^(-6)) {
+
   diff_beta <- 1
   iter <- 0
   temp_beta <- init_beta
-  
+
   stopSolve <- FALSE
   while( !is.nan(diff_beta) & stopSolve == FALSE & diff_beta<1000) {
-    
+
     etaL <- left_dmat %*% temp_beta
     etaR <- right_dmat %*% temp_beta
-    
+
     # Survival term
-    SL <- ifelse(tpos_ind == 0, 1, expit(-etaL))
-    SR <- ifelse(obs_ind == 0, 0, expit(-etaR))
+    SL <- ifelse(tpos_ind == 0, 1, rje::expit(-etaL))
+    SR <- ifelse(obs_ind == 0, 0, rje::expit(-etaR))
     SLSR <- SL - SR
-    
-    # understand why it's a 0 if lt == 0! (because expit(-eta) = S0 = 1 when t=0, so 1 - expit(-eta) = 0)
+
+    # expit(-eta) = S(t) = 1 when t=0, so 1 - expit(-eta) = 1 - S(t) = 0
     dGinvLdEta <- ifelse(tpos_ind == 0, 0, -expit(-etaL) * (1 - expit(-etaL)))
-    dGinvRdEta <- ifelse(obs_ind == 0, 0, -expit(-etaR) * (1 - expit(-etaR))) 
+    dGinvRdEta <- ifelse(obs_ind == 0, 0, -expit(-etaR) * (1 - expit(-etaR)))
     # put a 1 in here to remind that we've taken the ZL and ZR out of these two terms
     # these are all terms that will be put in the final matrix as sweep(t(Z), 2, toSweep) %*% Z
-    dEtaLdAlpha <- 1 
-    dEtaRdAlpha <- 1 
-    
+    dEtaLdAlpha <- 1
+    dEtaRdAlpha <- 1
+
     # second derivative terms
-    # understand why it's a 0 if lt == 0! (because again there's a 1- expit(-eta) term in there)
-    d2GinvLdEta2 <- ifelse(tpos_ind == 0, 0, (exp(-etaL) - exp(-2 * etaL)) / (1 + exp(-etaL))^3)  
+    # again, there's an S(T) and a 1 - S(T) in these terms
+    d2GinvLdEta2 <- ifelse(tpos_ind == 0, 0, (exp(-etaL) - exp(-2 * etaL)) / (1 + exp(-etaL))^3)
     d2GinvRdEta2 <- ifelse(obs_ind == 0, 0, (exp(-etaR) - exp(-2 * etaR)) / (1 + exp(-etaR))^3)
-    
+
     # dEtaLdAlpha is 1
     UalphaSweepL <- ifelse(tpos_ind == 0, 0, (dGinvLdEta * dEtaLdAlpha) / SLSR)
     UalphaSweepR <- ifelse(obs_ind == 0, 0, (dGinvRdEta * dEtaRdAlpha) / SLSR)
     # second part of score vector
-    Ualpha <- rowSums( sweep(t(ZL), MARGIN = 2, STATS = UalphaSweepL, FUN = "*") - 
+    Ualpha <- rowSums( sweep(t(ZL), MARGIN = 2, STATS = UalphaSweepL, FUN = "*") -
                          sweep(t(ZR), MARGIN = 2, STATS = UalphaSweepR, FUN = "*") )
-    
-    # Remember that A^TB != B^TA 
+
+    # Remember that A^TB != B^TA
     IaaSweepL <- ifelse(tpos_ind == 0, 0, (d2GinvLdEta2 * dEtaLdAlpha^2)/ SLSR) - UalphaSweepL^2
     IaaSweepR <- ifelse(obs_ind == 0, 0, -(d2GinvRdEta2 * dEtaRdAlpha^2) / SLSR) - UalphaSweepR^2
     IaaSweepBoth <- UalphaSweepL * UalphaSweepR
-    
+
     IaaL <- sweep(t(ZL), 2, IaaSweepL, FUN="*") %*% ZL
     IaaR <- sweep(t(ZR), 2, IaaSweepR, FUN="*") %*% ZR
     IaaBoth <- sweep(t(ZL), 2, IaaSweepBoth, FUN="*") %*% ZR + sweep(t(ZR), 2, IaaSweepBoth, FUN="*") %*% ZL
     # works, compared with iMat from ICSKAT_fit_null
     Iaa <- IaaL + IaaR + IaaBoth
-    
+
     # first part of score vector
     UbetaSweep <- (dGinvLdEta - dGinvRdEta) / SLSR
     Ubeta <- rowSums( sweep(t(xMat), MARGIN = 2, STATS = UbetaSweep, FUN = "*") )
-    
+
     # Ibb
     IbbSweep <- (d2GinvLdEta2 - d2GinvRdEta2) / SLSR - UbetaSweep^2
     Ibb <- sweep(t(xMat), 2, IbbSweep, FUN = "*") %*% xMat
-    
+
     # Iba
     IbaSweepL <- ifelse(tpos_ind == 0, 0, d2GinvLdEta2 * dEtaLdAlpha / SLSR - (dGinvLdEta - dGinvRdEta) * dGinvLdEta * dEtaLdAlpha / SLSR^2)
     IbaSweepR <- ifelse(obs_ind == 0, 0, d2GinvRdEta2 * dEtaRdAlpha / SLSR - (dGinvLdEta - dGinvRdEta) * dGinvRdEta * dEtaRdAlpha / SLSR^2)
     IbaL <- sweep(t(xMat), 2, IbaSweepL, FUN = "*") %*% ZL
     IbaR <- sweep(t(xMat), 2, IbaSweepR, FUN = "*") %*% ZR
     Iba <- IbaL - IbaR
-    
+
     # full iMat for "fixed effects"
     fullImat <- rbind(cbind(Ibb, Iba), cbind(t(Iba), Iaa))
     # full score vector
     fullU <- c(Ubeta, Ualpha)
-    
+
     # update
     # sometimes fullImat is singular
     solvedImat <- tryCatch(solve(fullImat), error=function(e) e)
@@ -116,10 +116,10 @@ ICSKAT_fit_null_PO <- function(init_beta, left_dmat, right_dmat, obs_ind, tpos_i
     temp_beta <- as.numeric(beta_new)
     iter <- iter + 1
     if (checkpoint) {cat("iter ", iter, "diff ", diff_beta, "\n")}
-    
+
     # stop?
     stopSolve <- ifelse(diff_beta < eps & sum(fullU^2) < length(fullU), TRUE, FALSE)
   }
-  
+
   return(list(beta_fit=beta_new, iter=iter, Itt=fullImat, err=0, errMsg=""))
 }
