d33269e11166f2d99f9589fbed525ce78d879f52
Ryan Sun <ryanrsun@gmail.com>
2021-03-01 11:38:08 -0600

    Removed unnecessary functions and added documentation

--- src/matrixMultCpp.cpp
+++ src/matrixMultCpp.cpp
@@ -1,21 +1,17 @@
 // [[Rcpp::depends(RcppEigen)]]
+//' matrixMultCpp.cpp
+//'
+//' Faster matrix multiplication for ICSKAT tests.
+//'
+//' @param A First matrix to be multiplied, will be transposed for t(A) %*% B
+//' @param B Second matrix to be multiplied
+//' @return C Product of A and B
+//' @export
+
 
 #include <RcppEigen.h>
 using namespace Eigen;
 using namespace std;
-
-// [[Rcpp::export]]
-SEXP eigenMapMatMultC(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B){
-  Eigen::MatrixXd C = A * B;
-  return Rcpp::wrap(C);
-}
-
-// [[Rcpp::export]]
-SEXP eigenMapMatMultCrossprod(const Eigen::Map<Eigen::MatrixXd> A){
-  const int n(A.cols());
-  MatrixXd AtA(MatrixXd(n, n).setZero().selfadjointView<Eigen::Lower>().rankUpdate(A.adjoint()));
-  return Rcpp::wrap(AtA);
-}
 
 // [[Rcpp::export]]
 SEXP eigenMapMatMultCrossTwo(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B){
